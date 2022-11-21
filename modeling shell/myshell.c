de098a4ca6b639ada02ad279109d1be3f9a23463
Artem Moor <moor.artem@gmail.com>
2022-11-21 23:15:16 +0300

    renaming

--- modeling shell/myshell.c
+++ modeling shell/myshell.c
@@ -7,31 +7,30 @@
 #include "parser.c"
 
 
-void free_all(words_array words, str command)
+void free_all(words_array words, str line)
 {
     int i;
     for (i = 0; words[i] != NULL; i++)
         free(words[i]);
     free(words);
-    free(command);
+    free(line);
 }
 
 
 int main(int argc, const str *argv)
 {
     FILE *input, *output;
-    words_array words;
-    str command;
+    str line;
     process_args(argc, argv, &input, &output);
 
-    while ((command = get_command(input)) != NULL)
+    while ((line = get_line(input)) != NULL)
     {
-        words = parse_command(command);
+        words_array words = parse_line(line);
         if (words[0] != NULL)
         {
             if (strcmp(words[0], "exit") == 0)
             {
-                free_all(words, command);
+                free_all(words, line);
                 break;
             }
             else if (strcmp(words[0], "cd") == 0)
@@ -45,19 +44,19 @@ int main(int argc, const str *argv)
                 if (pid == -1)
                 {
                     perror("fork failed");
-                    free_all(words, command);
+                    free_all(words, line);
                     return 1;
                 }
                 if (pid == 0)
                 {
                     execvp(words[0], words);
                     perror("execvp failed");
-                    free_all(words, command);
+                    free_all(words, line);
                     return 2;
                 }
                 else wait(NULL);
             }
         }
-        free_all(words, command);
+        free_all(words, line);
     }
 }
--- modeling shell/parser.c
+++ modeling shell/parser.c
@@ -51,10 +51,10 @@ void add_char_as_word(int c, words_array *ptr, int *word_count)
 }
 
 
-str get_command(FILE *stream)
+str get_line(FILE *stream)
 {
     int c, n = 0, quot_marks_flag = 0, eof_warning_flag = 0;
-    str command = (str)malloc(n + 1);
+    str line = (str)malloc(n + 1);
 
     char *username = getenv("USER");
     char *current_working_dir = getcwd(NULL, 0);
@@ -66,63 +66,63 @@ str get_command(FILE *stream)
     while ((c = fgetc(stream)) != '\n' && c != EOF)
     {
         n++;
-        command = realloc(command, n + 1);
-        command[n - 1] = (char)c;
+        line = realloc(line, n + 1);
+        line[n - 1] = (char)c;
         if (c == '"') quot_marks_flag ^= 1;
     }
-    command[n] = '\0';
+    line[n] = '\0';
     if (c == EOF)
     {
         if (n == 0)
         {
-            free(command);
+            free(line);
             return NULL;
         }
         else eof_warning_flag = 1;
     }
 
 
-    if (stream != stdin) puts(command);
+    if (stream != stdin) puts(line);
     if (quot_marks_flag)
     {
         fputs("ERROR: no closing quotation marks found.\n", stderr);
-        return get_command(stream);
+        return get_line(stream);
     }
     if (eof_warning_flag == 1)
     {
         fputs("WARNING: no newline at the end of the file.\n", stderr);
         ungetc(EOF, stream);
     }
-    return command;
+    return line;
 }
 
 
-words_array parse_command(str command)
+words_array parse_line(str line)
 {
     int i = 0, word_count = 0;
     words_array words = (words_array)malloc(sizeof(str) * (word_count + 1));
-    while (command[i] != '\0')
+    while (line[i] != '\0')
     {
         int letter_count = 0;
         str word = (str)malloc(letter_count + 1);
         enum sep_type sep;
 
-        while (command[i] == ' ') i++;
-        while ((sep = sep_check(command[i])) == not_separator)
+        while (line[i] == ' ') i++;
+        while ((sep = sep_check(line[i])) == not_separator)
         {
-            if (command[i] == '"')
+            if (line[i] == '"')
             {
                 i++;
-                while (command[i] != '"')
+                while (line[i] != '"')
                 {
-                    add_letter(command[i], &word, &letter_count);
+                    add_letter(line[i], &word, &letter_count);
                     i++;
                 }
                 i++;
             }
             else
             {
-                add_letter(command[i], &word, &letter_count);
+                add_letter(line[i], &word, &letter_count);
                 i++;
             }
         }
@@ -134,17 +134,17 @@ words_array parse_command(str command)
         }
         else free(word);
 
-        if (sep == special_separator && command[i] == command[i + 1])
+        if (sep == special_separator && line[i] == line[i + 1])
         {
             word = (str)malloc(3);
-            word[0] = word[1] = command[i];
+            word[0] = word[1] = line[i];
             word[2] = '\0';
             add_word(word, &words, &word_count);
             i += 2;
         }
         else if (sep == ordinary_separator || sep == special_separator)
         {
-            add_char_as_word(command[i], &words, &word_count);
+            add_char_as_word(line[i], &words, &word_count);
             i++;
         }
     }
