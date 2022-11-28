45ee156a4a8137e1061d6bb96f95123543bba5f7
Artem Moor <moor.artem@gmail.com>
2022-11-28 23:56:41 +0300

    updated

--- modeling shell/cli.c
+++ modeling shell/cli.c
@@ -9,35 +9,30 @@ https://www.gnu.org/software/libc/manual/html_node/Getopt.html
 #include <stdlib.h>
 
 
-void process_args(int argc, char *const *argv, FILE **input, FILE **output)
+void get_options(int argc, char *const *argv, FILE **input)
 {
     int i, c, option_index;
-    char *filename_r = NULL, *filename_w = NULL;
+    char *filename_r = NULL;
     struct option long_options[] = {
         {"input", required_argument, 0, 'i'},
-        {"output", required_argument, 0, 'o'},
         {"help", no_argument, 0, 'h'},
         {0, 0, 0, 0}
     };
 
     for (;;)
     {
-        c = getopt_long_only(argc, argv, "i:o:h", long_options, &option_index);
+        c = getopt_long_only(argc, argv, "i:h", long_options, &option_index);
         if (c == -1) break;
         switch(c)
         {
             case 'i':
                 filename_r = optarg;
                 break;
-            case 'o':
-                filename_w = optarg;
-                break;
             case 'h':
                 puts(
                     "List of available options:\n"
                     " -h,  --help            | see this help message\n"
-                    " -i,  --input           | specify input file\n"
-                    " -o,  --output          | specify output file"
+                    " -i,  --input           | specify input file"
                 );
                 exit(0);
             case '?':
@@ -53,17 +48,6 @@ void process_args(int argc, char *const *argv, FILE **input, FILE **output)
         {
             perror(filename_r);
             exit(1);
-        }
-    }
-
-    if(!filename_w) *output = stdout;
-    else
-    {
-        *output = fopen(filename_w, "w");
-        if (!*output)
-        {
-            perror(filename_w);
-            exit(2);
         }
     }
 
--- modeling shell/myshell.c
+++ modeling shell/myshell.c
@@ -10,23 +10,30 @@
 #include "parser.c"
 
 
-typedef struct Pipeline
+typedef struct Pipe_elem
 {
     words_array words;
-    struct Pipeline *next;
+    struct Pipe_elem *next;
     int fd[2];
-} Pipeline;
+} Pipe_elem, *Pipeline;
 
 
-Pipeline *make_pipeline(words_array words)
+typedef struct pid_list_elem
 {
-    Pipeline *pipeline = NULL, **tmp = &pipeline;
+    struct pid_list_elem *next;
+    int pid;
+} pid_list_elem, *pid_list;
+
+
+Pipeline make_pipeline(words_array words)
+{
+    Pipeline pipeline = NULL, *tmp = &pipeline;
     int i = 0;
     for(;;)
     {
         int n = 0;
         words_array wa = (words_array)malloc(sizeof(str));
-        *tmp = (Pipeline*)malloc(sizeof(Pipeline));
+        *tmp = (Pipeline)malloc(sizeof(Pipe_elem));
         (*tmp)->fd[0] = (*tmp)->fd[1] = -1;
         while (words[i] != NULL && strcmp(words[i], "|") != 0)
         {
@@ -43,6 +50,11 @@ Pipeline *make_pipeline(words_array words)
                 (*tmp)->fd[0] = in;
                 free(words[i]); free(words[i + 1]);
                 i += 2;
+            }
+            else if (strcmp(words[i], "&") == 0)
+            {
+                free(words[i]);
+                i++;
             }
             else
             {
@@ -66,11 +78,11 @@ Pipeline *make_pipeline(words_array words)
 }
 
 
-void free_pipeline(Pipeline *pipeline)
+void free_pipeline(Pipeline pipeline)
 {
     while (pipeline != NULL)
     {
-        Pipeline *tmp = pipeline;
+        Pipeline tmp = pipeline;
         pipeline = pipeline->next;
         int i;
         for (i = 0; tmp->words[i] != NULL; i++)
@@ -78,6 +90,33 @@ void free_pipeline(Pipeline *pipeline)
         free(tmp->words);
         free(tmp);
     }
+}
+
+
+void push_pid(pid_list *pl, int pid)
+{
+    pid_list tmp = (pid_list)malloc(sizeof(pid_list_elem));
+    tmp->next = *pl;
+    tmp->pid = pid;
+    *pl = tmp;
+}
+
+
+int pop_pid(pid_list *pl, int pid)
+{
+    pid_list *tmp = pl;
+    while (*tmp != NULL)
+    {
+        if ((*tmp)->pid == pid)
+        {
+            pid_list tmp2 = *tmp;
+            *tmp = (*tmp)->next;
+            free(tmp2);
+            return 1;
+        }
+        else tmp = &((*tmp)->next);
+    }
+    return 0;
 }
 
 
@@ -92,13 +131,14 @@ int set_background_flag(words_array words)
 
 int main(int argc, const str *argv)
 {
-    FILE *input, *output;
+    FILE *input;
     str line;
-    process_args(argc, argv, &input, &output);
+    pid_list bckg = NULL, done = NULL;
+    get_options(argc, argv, &input);
 
     while ((line = get_line(input)) != NULL)
     {
-        int background_flag;
+        int background_flag, pid;
         words_array words = parse_line(line);
         free(line);
         background_flag = set_background_flag(words);
@@ -112,7 +152,7 @@ int main(int argc, const str *argv)
             continue;
         }
 
-        Pipeline *pipeline = make_pipeline(words);
+        Pipeline pipeline = make_pipeline(words);
         if (pipeline->words[0] != NULL)
         {
             if (strcmp(pipeline->words[0], "exit") == 0)
@@ -129,8 +169,9 @@ int main(int argc, const str *argv)
             }
             else
             {
-                Pipeline *tmp = pipeline;
-                int fd_in_backup = dup(0), fd[2], pid;
+                Pipeline tmp = pipeline;
+                pid_list cur_pids = NULL;
+                int fd_in_backup = dup(0), fd[2];
                 while (tmp != NULL)
                 {
                     pipe(fd);
@@ -162,12 +203,42 @@ int main(int argc, const str *argv)
                     }
                     dup2(fd[0], 0);
                     close(fd[0]); close(fd[1]);
+                    push_pid(&cur_pids, pid);
                     tmp = tmp->next;
                 }
-                while(wait(NULL) != -1);
+                if (background_flag)
+                {
+                    while (cur_pids != NULL)
+                    {
+                        push_pid(&bckg, cur_pids->pid);
+                        pop_pid(&cur_pids, cur_pids->pid);
+                    }
+                }
+                else
+                {
+                    while (cur_pids != NULL)
+                    {
+                        pid = wait(NULL);
+                        if (!pop_pid(&cur_pids, pid))
+                        {
+                            pop_pid(&bckg, pid);
+                            push_pid(&done, pid);
+                        }
+                    }
+                }
                 dup2(fd_in_backup, 0);
                 close(fd_in_backup);
             }
+        }
+        while ((pid = waitpid(-1, NULL, WNOHANG)) != 0 && pid != -1)
+        {
+            pop_pid(&bckg, pid);
+            push_pid(&done, pid);
+        }
+        while (done != NULL)
+        {
+            printf("done: pid = %d\n", done->pid);
+            pop_pid(&done, done->pid);
         }
         free_pipeline(pipeline);
     }
